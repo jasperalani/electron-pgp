59228345b534c17edcc4efa74a9dc40a29eb8fd8
Jasper Alani <jasperalani@gmail.com>
2020-10-23 18:28:27 +0100

    Update variable names in env.js

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ if(environmentArgument === 'production' || environmentArgument === 'development'
   // Set environment
   const js = `
   /*** AUTO GENERATED ENVIRONMENT FILE DO NOT EDIT ***/
-  module.exports.ENV = '` + environmentArgument + `';
-  module.exports.CREATION_DATETIME = '` + new Date() + `';
+  module.exports.ENVIRONMENT = '` + environmentArgument + `';
+  module.exports.ENVIRONMENT_CREATION_DATETIME = '` + new Date() + `';
   /*** VARIABLES MUST BE ACCESSED USING ES6 MODULE SYNTAX ***/
   `
   fs.writeFile('env.js', js, function (err) {
